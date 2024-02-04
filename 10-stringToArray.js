741ebc076f28b5bb246bd39984a621d1e71967fb
SahalFajri <sahalfajri879@gmail.com>
2024-02-05 03:10:23 +0700

    day 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,10 @@
+// WPU Coding Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+function stringToArray(string) {
+  return string.split(' ');
+}
+
+console.log(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+console.log(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
